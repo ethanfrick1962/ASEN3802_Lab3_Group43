ef3bd36389f9aecaa91407a2c90fe0b77a1e6b41
Ethan Frick <ethanfrick1962@outlook.com>
2025-11-18 11:54:04 -0700

    PLLT boilerplate

---
+++ PLLT.m
@@ -0,0 +1,24 @@
+function [e, c_L, c_Di] = PLLT(b, a0_t, a0_r, c_t, c_r, aero_t, aero_r, geo_t, geo_r, N)
+%PLLT Summary of this function goes here
+%   Detailed explanation goes here
+arguments (Input)
+    b
+    a0_t
+    a0_r
+    c_t
+    c_r
+    aero_t
+    aero_r
+    geo_t
+    geo_r
+    N
+end
+
+arguments (Output)
+    e
+    c_L
+    c_Di
+end
+
+
+end
